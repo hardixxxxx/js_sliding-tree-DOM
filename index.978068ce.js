0b1e783d3d9ccb7ac9482b86af17c95b750316ef
Anton Polishchuk <hardixxxxx@gmail.com>
2024-06-19 02:12:45 +0200

    make build

---
+++ index.978068ce.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".tree");e.onclick=function(e){var n=e.target.nextSibling;n&&(n.hidden=!n.hidden)},e.querySelectorAll("li").forEach(function(e){var n=document.createElement("span");e.prepend(n),n.append(n.nextSibling)});
+//# sourceMappingURL=index.978068ce.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+"use strict";
+const tree = document.querySelector(".tree");
+tree.onclick = (ev)=>{
+    const list = ev.target.nextSibling;
+    // const test = findUl(ev.target);
+    if (list) list.hidden = !list.hidden;
+};
+function spanElements() {
+    const liElements = tree.querySelectorAll("li");
+    liElements.forEach((li)=>{
+        const span = document.createElement("span");
+        li.prepend(span);
+        span.append(span.nextSibling);
+    });
+}
+spanElements(); // function findUl(node) {
+ //   const father = node.parentNode;
+ //   const ul = father.querySelector('ul');
+ //   if (ul) {
+ //     return ul;
+ //   } else {
+ //     findUl(father);
+ //   }
+ // }
+
+//# sourceMappingURL=index.f75de5e1.js.map
